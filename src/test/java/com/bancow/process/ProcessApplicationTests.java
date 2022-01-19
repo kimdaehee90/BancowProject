765e07ea5b93050142d981ee142888a0b5e0947b
cjsrhd94 <cjsrhd94@gmail.com>
2022-01-19 20:19:32 +0900

    initial commit

---
+++ src/test/java/com/bancow/process/ProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancow.process;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
